79250750d73147adff81374f1a4f50be92be4570
John Berryman <john@vividcortex.com>
2014-10-07 13:19:46 -0400

    Added documentation

---
+++ utils.go
@@ -0,0 +1,40 @@
+// utils.go contains common code that is not necessary to use with siesta,
+// but is often helpful.
+
+package siesta
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// JsonResponseWriter is a common "post" middleware that writes the
+// response as JSON and places the status code in the header. It looks
+// for the status code in context[status_code] (must be castable to int).
+// And it looks for response in context[response].
+func JsonResponseWriter(status_code, response string) func(c Context, w http.ResponseWriter, r *http.Request, quit func()) {
+	return func(c Context, w http.ResponseWriter, r *http.Request, quit func()) {
+		enc := json.NewEncoder(w)
+
+		// If we have a status code set in the context,
+		// send that in the header.
+		//
+		// Go defaults to 200 OK.
+		statusCode := c.Get(status_code)
+		if statusCode != nil {
+			statusCodeInt := statusCode.(int)
+			w.WriteHeader(statusCodeInt)
+		}
+
+		// Check to see if we have some sort of response.
+		response := c.Get(response)
+		if response != nil {
+			// We'll encode it as JSON without knowing
+			// what it exactly is.
+			enc.Encode(response)
+		}
+
+		// We're at the end of the middleware chain, so quit.
+		quit()
+	}
+}
