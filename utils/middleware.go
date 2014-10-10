5e4296c9a54970db49d90a8afc7638ceeb01c5a5
John Berryman <john@vividcortex.com>
2014-10-09 22:11:48 -0400

    implementing requested changes

--- utils/middleware.go
+++ utils/middleware.go
@@ -1,9 +1,11 @@
 // utils.go contains common code that is not necessary to use with siesta,
 // but is often helpful.
 
-package siesta
+package util
 
 import (
+	"github.com/VividCortex/siesta"
+
 	"encoding/json"
 	"log"
 	"net/http"
@@ -11,24 +13,24 @@ import (
 
 // JsonResponseWriter is a common "post" middleware that writes the
 // response as JSON and places the status code in the header. It looks
-// for the status code in context[status_code] (must be castable to int).
+// for the status code in context[statusCodeStr] (must be castable to int).
 // And it looks for response in context[response].
-func JsonResponseWriter(status_code_str, response_str string) func(c Context, w http.ResponseWriter, r *http.Request, quit func()) {
-	return func(c Context, w http.ResponseWriter, r *http.Request, quit func()) {
+func JsonResponseWriter(statusCodeStr, responseStr string) func(c siesta.Context, w http.ResponseWriter, r *http.Request, quit func()) {
+	return func(c siesta.Context, w http.ResponseWriter, r *http.Request, quit func()) {
 		enc := json.NewEncoder(w)
 
 		// If we have a status code set in the context,
 		// send that in the header.
 		//
 		// Go defaults to 200 OK.
-		statusCode := c.Get(status_code_str)
+		statusCode := c.Get(statusCodeStr)
 		if statusCode != nil {
 			statusCodeInt := statusCode.(int)
 			w.WriteHeader(statusCodeInt)
 		}
 
 		// Check to see if we have some sort of response.
-		response := c.Get(response_str)
+		response := c.Get(responseStr)
 		if response != nil {
 			// We'll encode it as JSON without knowing
 			// what it exactly is.
@@ -37,6 +39,5 @@ func JsonResponseWriter(status_code_str, response_str string) func(c Context, w 
 				log.Println("couldn't encode response:", err)
 			}
 		}
-
 	}
 }
